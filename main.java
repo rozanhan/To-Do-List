9088ceace106ccc99c3cfcd5ea931f95812a04a5
rozanhan <rozanhan@student.ub.ac.id>
2024-11-05 19:37:14 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,14 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
 public class main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[]a = new String[8];
+
         
     }
 }
